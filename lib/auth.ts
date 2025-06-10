c7ddc6aeec4338b84a77da12a46d9c10eff6a7b5
Krishna <gohilkrishna9004@gmail.com>
2025-06-10 22:11:14 +0530

    Fixed a login issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -47,6 +47,8 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           email: user.email,
           name: user.name,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
         };
       },
     }),
